08ad56d46284950a2f84077278b3b41eb88a702c
Mateus Bragatto <mateusbragatto@estudante.ufscar.br>
2023-12-05 22:24:52 +0000

    Adding TOS version (p4, sender, receiver)

---
+++ packet/send.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys
+import time
+import random
+from int_hdrs import *
+
+def main():
+    identification=random.randint(1,65535)
+
+    # iface = 'h0-eth0'
+
+    # pkt =  Ether(dst='00:aa:bb:00:00:00', src=get_if_hwaddr('eth0'), type=8224);
+    pkt =  Ether(dst='00:04:00:00:00:01', src=get_if_hwaddr('eth0'), type=0x800);   #2048 works
+
+    # try:
+    #     pkt = pkt / SourceRoute(nrouteid=1915945086214166489341648836749912) #borda so faz essa linha - alem do routeID, inserir o cabecalho INT
+    # except ValueError:
+    #     pass
+
+    # pkt = pkt / IP(id=identification)/IntHeader(ver=2, d=0, e=0, m=0, r=0, hop_ml=10, remaining_hop_count=0, instruction_bitmap=56832, domain_specific_id=0x0000)
+
+    pkt = pkt/IP(id=identification, src= get_if_addr('eth0'), dst='10.0.1.2', tos = 55)/UDP(dport=1234, sport=random.randint(49152,65535)) / sys.argv[1]  #adicionando um header de udp para evitar resposta ICMP
+
+    try:
+        pkt.show2()
+        time.sleep(1)
+        sendp(pkt, iface='eth0')
+        time.sleep(1)
+    except KeyboardInterrupt:
+        sys.exit()
+
+if __name__ == '__main__':
+    main()
